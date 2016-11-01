53892d8ba6496034f71e647d289b1386acf41471
tunnckoCore <mameto_100@mail.bg>
2016-11-01 07:44:19 +0200

    chore(init): scaffold

---
+++ test.js
@@ -0,0 +1,8 @@
+/*!
+ * charlike <https://github.com/tunnckoCore/charlike>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
