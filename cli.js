f4e70aec537defd0f514916a475ade4b00b66605
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-02 17:36:00 +0200

    major: bring in the CLI, interop for the cjs
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

---
+++ cli.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const proc = require('process');
+const getPkg = require('@tunnckocore/package-json').default;
+
+const cli = require('mri')(proc.argv.slice(2), {
+  alias: {
+    owner: 'o',
+    name: 'n',
+    desc: 'd',
+    repo: 'r',
+    engine: 'e',
+    licenseStart: 'l',
+    locals: 'L',
+    templates: 't',
+    help: 'h',
+    version: 'v',
+  },
+});
+
+const charlike = require('./index');
+
+proc.title = 'charlike-cli';
+
+const name = cli._[0] || cli.name;
+const desc = cli._[1] || cli.desc;
+
+delete cli._;
+
+function get(pkgName, field = 'version') {
+  return getPkg(pkgName).then((pkg) => pkg[field]);
+}
+
+async function showHelp(status = 0) {
+  console.log(`
+  ${await get('charlike', 'description')}
+  (charlike v${await get('charlike')})
+  Usage
+    $ charlike <name> <description> [flags]
+
+  Common Flags
+    --help            Show this output
+    --version         Show version
+
+  Options
+    --owner, -o       Project github owner - username or organization
+    --name, -n        Name of the project, same as to pass first param
+    --desc, -d        Project description, same as to pass second param
+    --repo, -r        Repository pattern like username/projectName
+    --engine, -e      Engine to be used, j140 by default
+    --licenseStrt, -l Context to pass to template files (support dot notation)
+    --locals, -L      Context to pass to template files (support dot notation)
+    --templates, -t   Path to templates folder
+    --cwd             Folder to be used as current working dir
+
+  Examples
+    $ charlike my-awesome-project 'some cool description'
+    $ charlike minibase-data 'we are awesome' --owner node-minibase
+    $ charlike --desc 'abc description here' -n beta-trans -o gulpjs
+
+  Issues: ${await get('charlike', 'homepage')}
+  `);
+
+  if (status !== 1) {
+    throw new Error('foo');
+  }
+
+  proc.exit(status);
+}
+
+if (cli.help) {
+  showHelp();
+}
+
+if (!name || !desc) {
+  showHelp(1).catch(() => proc.exit(1));
+} else {
+  cli.description = desc;
+
+  /* eslint-disable promise/always-return */
+
+  charlike(name, desc, cli)
+    .then((dest) => {
+      console.log(`Project "${name}" scaffolded to "${dest}"`);
+    })
+    .catch((err) => {
+      /* istanbul ignore next */
+      console.error(`Sorry, some error occured!`);
+      /* istanbul ignore next */
+      console.error(`ERROR: ${err.message}`);
+      /* istanbul ignore next */
+      proc.exit(1);
+    });
+}
